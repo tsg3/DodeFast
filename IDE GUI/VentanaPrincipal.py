fc8d1e01236ad7aabe625ebd12feb91f6f943681
Jose Ortega <ortega.josant@gmail.com>
2019-03-08 14:54:58 -0600

    Al guardar se guarda un único archivo con lo escrito en la ventana superior y si acciona correr se obtiene el texto del código guardado, se debe guardar primero antes de poder correr lo escrito

--- IDE GUI/VentanaPrincipal.py
+++ IDE GUI/VentanaPrincipal.py
@@ -3,10 +3,17 @@ from tkinter import *
 
 def obtenerCodigo():
     texto = textCommand.get("1.0", END)
+    archivoCodigo = open("C:/Users/jorte/Documents/Compiladores e Intérpretes/DodeFast/codigosPrueba/codigo.txt", "w")
+    archivoCodigo.write(texto)
+    archivoCodigo.close()
 
-    print(texto)
-
-
+def correrCodigo():
+    archivoCodigo = open("C:/Users/jorte/Documents/Compiladores e Intérpretes/DodeFast/codigosPrueba/codigo.txt", "r")
+    codigo = archivoCodigo.read()
+    archivoCodigo.close()
+    textTerminal.config(state=NORMAL)
+    textTerminal.insert(END, codigo)
+    textTerminal.config(state=DISABLED)
 # Ventana principal
 root = Tk()
 root.geometry("1280x640+5+5")
@@ -24,7 +31,9 @@ scrollText.place(in_=textCommand, relx=1, relheight=1, bordermode="outside")
 scrollText.config()
 
 # - Terminal
-textTerminal = Text(root, height=10, width=155, bg="white", bd=3, state=DISABLED)
+textTerminal = Text(root, height=10, width=155, bg="white", bd=3)
+textTerminal.insert(END, ">> DodeFast IDE\n")
+textTerminal.config(state=DISABLED)
 textTerminal.place(x=10, y=450)
 scrollTerminal = Scrollbar(root, command=textTerminal.yview)
 textTerminal.config(yscrollcommand=scrollTerminal.set)
@@ -33,7 +42,7 @@ scrollTerminal.place(in_=textTerminal, relx=1, relheight=1, bordermode="outside"
 # - Botones
 
 botonGuardar = Button(root, text="Guardar", command=lambda: obtenerCodigo())
-botonCorrer = Button(root, text="Correr")
+botonCorrer = Button(root, text="Correr", command=lambda: correrCodigo())
 botonNuevo = Button(root, text="Nuevo")
 botonAbrir = Button(root, text="Abrir")
 
